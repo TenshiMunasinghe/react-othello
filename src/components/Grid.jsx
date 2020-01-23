5c7641e47c23908083c500d9afd7bc4097a0a186
shadow-chadow <56468853+shadow-chadow@users.noreply.github.com>
2020-01-23 19:41:20 +0530

    added animation

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
+import AnimateOnChange from "react-animate-on-change";
 
-export default function Grid({x, y, id, content, handleChange, handleHover}) {
-	return (
-		<div
-			className='grid-item'
-			id={id}
-			onClick={() => {
-				handleChange(y, x);
-			}}
-			onMouseOver={e => {
-				handleHover(y, x, e);
-			}}>
-			{content}
-		</div>
-	);
+export default class Grid extends Component {
+	state = {
+		content: "",
+		isChanged: false
+	};
+
+	static getDerivedStateFromProps(nextProps, prevState) {
+		const isChanged = nextProps.content !== prevState.content;
+		return {content: nextProps.content, isChanged};
+	}
+
+	render() {
+		console.log(this.state.isChanged);
+
+		const {x, y, id, handleChange, handleHover} = this.props;
+		return (
+			<div
+				className='grid-item'
+				id={id}
+				onClick={() => {
+					handleChange(y, x);
+				}}
+				onMouseOver={e => {
+					handleHover(y, x, e);
+				}}>
+				<AnimateOnChange
+					baseClassName='content'
+					animationClassName='content-changed'
+					animate={this.state.isChanged}>
+					{this.state.content}
+				</AnimateOnChange>
+			</div>
+		);
+	}
 }
